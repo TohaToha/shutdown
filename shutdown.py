65690538340f0c544414f390211dfc0ee865e0d6
TohaToha <nikus032003@rambler.ru>
2018-06-03 01:34:07 +0300

    Add files via upload
    Исправлена не интуитивность интерфейса

--- shutdown.py
+++ shutdown.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import socket
 import subprocess
 sock = socket.socket()
 sock.bind(('', 9090))
-f = open("test.html",'rb')
+f = open("GUI.html",'rb')
 page = f.read()
 f.close()
 
@@ -10,15 +12,12 @@ while True:
    sock.listen(1)
    conn, addr = sock.accept()
    data = conn.recv(1024)
-   if data[5:10] == b'sleep':#sleep
+   if data[5:6] == b' ':
+      conn.send(page)
+   if data[5:10] == b'sleep':
       subprocess.Popen("rundll32.exe Powrprof.dll,SetSuspendState")
-   if data[5:16] == b'hibernation':#hibernation
+   if data[5:16] == b'hibernation':
       subprocess.Popen("shutdown -h")
    if data[5:13] == b'shutdown':
       subprocess.Popen("shutdown -s -t 0")
-   if data[5:9] == b'help':
-      text = b'Available commands >>> sleep, hibernation, shutdown'
-      conn.send(text + b' ' * (1024-len(text)))
-   if data[5:9] == b'test':
-      conn.send(page)
    conn.close()
